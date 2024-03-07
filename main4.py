0f55aca07734924192fd5baaf5b5050541272af5
jgrynczewski <jgrynczewski@gmail.com>
2024-03-07 19:37:02 +0100

    mysql-connector-python ddl - create tabels

--- main4.py
+++ main4.py
@@ -28,19 +28,41 @@ PASSWORD = 'admin'
 DB = 'shop'
 
 CREATE_CLIENT_TABLE = """
-CREATE TABLE IF NOT EXISTS client(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS client (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100)
 )
 """
+CREATE_PRODUCT_TABLE = """
+CREATE TABLE IF NOT EXISTS product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL
+)
+"""
 
+CREATE_ORDER_DETAIL_TABLE = """
+CREATE TABLE IF NOT EXISTS order_detail (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    client_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (client_id) REFERENCES client(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+)
+"""
 
 try:
     with connect(user=USER, password=PASSWORD, database=DB) as cnx:
         with cnx.cursor() as cursor:
             cursor.execute(CREATE_CLIENT_TABLE)
+            cursor.execute(CREATE_PRODUCT_TABLE)
+            cursor.execute(CREATE_ORDER_DETAIL_TABLE)
+
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your username or password.")
@@ -52,4 +74,3 @@ except mysql.connector.Error as err:
         print("An error occured\n", err)
 else:
     print("Done.")
-
