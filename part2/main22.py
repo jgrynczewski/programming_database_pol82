57134baded9f53dcf9473bd9ece5b19d43f32b29
jgrynczewski <jgrynczewski@gmail.com>
2024-03-13 21:09:13 +0100

    sqlalchemy core select, sortowanie

---
+++ part2/main22.py
@@ -0,0 +1,25 @@
+# SQLAlchemy Expression Language (R z CRUD dla DQL) funkcja select, sortowanie
+# Zadanie 8: Sortowanie
+#
+# Wykonaj zapytanie SELECT z warunkiem WHERE, aby wybrać pracowników o pensji większej niż 4500.
+# Wynik posortuj alfabetycznie po kolumnie last_name
+
+
+
+from sqlalchemy import create_engine, select
+
+from tables import employee_table
+
+# napis połączeniowy (ang. connection string)
+conn_str = "mysql+mysqlconnector://root:admin@localhost/company_db"
+engine = create_engine(conn_str, echo=True)
+
+stmt = select(employee_table).where(employee_table.c.salary > 4500).order_by(employee_table.c.last_name)
+
+with engine.connect() as conn:
+    cursor_result = conn.execute(stmt)  # <class 'sqlalchemy.engine.cursor.CursorResult'>
+
+    for row in cursor_result:
+        print(f"({row.id}) {row.first_name} {row.last_name} - {row.position} [{row.salary}]")
+
+print("Done.")
