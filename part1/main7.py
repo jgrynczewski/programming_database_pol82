e3c62a5927e63fecefd859ea9638cfbc4d56d1ea
jgrynczewski <jgrynczewski@gmail.com>
2024-03-18 16:22:02 +0100

    erd for exe

--- part1/main7.py
+++ part1/main7.py
@@ -19,16 +19,23 @@ product_name = input("Poodaj nazwę produktu: ")
 product_price = int(input("Podaj cenę produktu: "))
 stock_quantity = input("Podaj liczbę produktów na stanie: ")
 
-INSERT_PRODUCT = """
-INSERT INTO product (name, price, stock_quantity) VALUE
-(%s, %s, %s);
-"""
+# INSERT_PRODUCT = """
+# INSERT INTO product (name, price, stock_quantity) VALUE
+# (%s, %s, %s);
+# """
 
+# """'telefon', 100, 3); DELETE TABLE order_detail;--"""
+"""telefon', 100, 3);DELETE TABLE order_detail;--"""
+INSERT_PRODUCT = f"""
+INSERT INTO product (name, price, stock_quantity) VALUE
+('{product_name}', {product_price}, {stock_quantity});
+"""
 
 try:
     with connect(user=USER, password=PASSWORD, database=DB) as cnx:
         with cnx.cursor() as cursor:
-            cursor.execute(INSERT_PRODUCT, [product_name, product_price, stock_quantity])
+            # cursor.execute(INSERT_PRODUCT, [product_name, product_price, stock_quantity])
+            cursor.execute(INSERT_PRODUCT, multi=True)
             cnx.commit()
 
 except mysql.connector.Error as err:
