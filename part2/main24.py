61c25d7c819720d31d3a9ef9c761be2a505e6a19
jgrynczewski <jgrynczewski@gmail.com>
2024-03-18 18:04:32 +0100

    zad 24 fix

--- part2/main24.py
+++ part2/main24.py
@@ -3,7 +3,7 @@
 #
 # Wykonaj zapytanie SELECT grupujące pracowników według ich stanowiska i wyświetlające liczbę osób na danym stanowsku.
 
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, select, func
 
 from tables import employee_table
 
@@ -11,8 +11,11 @@ from tables import employee_table
 conn_str = "mysql+mysqlconnector://root:admin@localhost/company_db"
 engine = create_engine(conn_str, echo=True)  # lazy initialization (leniwe inicjalizowanie)
 
-# tutaj jest błąd, w poniedziałek naprawimy
-stmt = employee_table.select(func.count(employee_table.c.position)).group_by(employee_table.c.position)
+# Sposób I (na bazie funkcji select)
+# stmt = select(employee_table.c.position, func.count()).group_by(employee_table.c.position)
+
+# Sposób II (na bazie metody select)
+stmt = employee_table.select().with_only_columns(employee_table.c.position, func.count()).group_by(employee_table.c.position)
 
 with engine.connect() as conn:
     cursor_result = conn.execute(stmt)  # <class 'sqlalchemy.engine.cursor.CursorResult'>
