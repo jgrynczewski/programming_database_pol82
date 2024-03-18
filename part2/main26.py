4524dc29808dd0fa4dfd3e534679ba0c6e693f7b
jgrynczewski <jgrynczewski@gmail.com>
2024-03-18 18:27:31 +0100

    zad 26

---
+++ part2/main26.py
@@ -0,0 +1,43 @@
+# SQLAlchemy Expression Language (C z CRUD dla DML)  Funkcja insert
+# Zadanie 3: Wstawianie danych (dml)
+#
+# Dodaj co najmniej trzy rekordy do tabeli "employee".
+# Przykładowe dane:
+# {'first_name': 'John', 'last_name': 'Doe', 'position': 'Manager', 'salary': 5000.00},
+# {'first_name': 'Jane', 'last_name': 'Smith', 'position': 'Developer', 'salary': 4000.00},
+# {'first_name': 'Alice', 'last_name': 'Johnson', 'position': 'HR', 'salary': 4500.00}
+
+#
+
+from sqlalchemy import create_engine, insert, update
+
+from tables import employee_table, department_table
+
+# napis połączeniowy (ang. connection string)
+conn_str = "mysql+mysqlconnector://root:admin@localhost/company_db"
+engine = create_engine(conn_str, echo=True)
+
+insert_employees_query = insert(employee_table).values([
+    {'first_name': 'John', 'last_name': 'Doe', 'position': 'Manager', 'salary': 5000.00},
+    {'first_name': 'Jane', 'last_name': 'Smith', 'position': 'Developer', 'salary': 4000.00},
+    {'first_name': 'Alice', 'last_name': 'Johnson', 'position': 'HR', 'salary': 4500.00}
+])
+
+insert_departments_query = insert(department_table).values([
+    {'name':'HR', 'manager_id': 2},
+    {'name':'IT', 'manager_id': 3}
+])
+
+update_employee1_query = update(employee_table).where(employee_table.c.id == 1).values(department_id=2)
+update_employee2_query = update(employee_table).where(employee_table.c.id == 2).values(department_id=1)
+update_employee3_query = update(employee_table).where(employee_table.c.id == 3).values(department_id=2)
+
+with engine.connect() as conn:
+    conn.execute(insert_employees_query)
+    conn.execute(insert_departments_query)
+    conn.execute(update_employee1_query)
+    conn.execute(update_employee2_query)
+    conn.execute(update_employee3_query)
+    conn.commit()
+
+print("Done.")
--- part2/tables.py
+++ part2/tables.py
@@ -1,6 +1,18 @@
-from sqlalchemy import MetaData, Table, Column, Integer, Float, String
+from sqlalchemy import MetaData, Table, Column, Integer, Float, String, ForeignKey
 
 metadata = MetaData()
+# wersja do zadania 25
+# employee_table = Table(
+#     'employee',
+#     metadata,
+#     Column('id', Integer, primary_key=True),
+#     Column('first_name', String(50)),
+#     Column('last_name', String(100)),
+#     Column('position', String(100)),
+#     Column('salary', Float)
+# )
+
+# wersja od zadania 25
 employee_table = Table(
     'employee',
     metadata,
@@ -8,5 +20,14 @@ employee_table = Table(
     Column('first_name', String(50)),
     Column('last_name', String(100)),
     Column('position', String(100)),
-    Column('salary', Float)
+    Column('salary', Float),
+    Column('department_id', Integer, ForeignKey("department.id")),
+)
+
+department_table = Table(
+    'department',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(100)),
+    Column('manager_id', Integer, ForeignKey('employee.id'), nullable=False)
 )
